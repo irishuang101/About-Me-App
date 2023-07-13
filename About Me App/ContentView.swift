e7ab3f9d3338dba29da5db7b67a6df9337ba129a
irishuang101 <26huangi49@stu.smuhsd.org>
2023-07-13 14:02:48 -0700

    stuff

--- About Me App/ContentView.swift
+++ About Me App/ContentView.swift
@@ -4,23 +4,44 @@
 //
 //  Created by Scholar on 7/13/23.
 //
+// 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
+
+// for git
+
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var textTitle = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack(alignment: .leading, spacing: 20.0) {
+            Image("Flower")
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .cornerRadius(1000)
+                .padding(20)
+            
+            Text("Iris Huang")
+                .font(.title)
+                .fontWeight(.bold)
+                .padding(20)
+            
+            Button("Funfact!"){
+                textTitle = "I can play the piano. I also enjoy reading in my free time!"
+            }
+            .font(.title2)
+            .buttonStyle(.borderedProminent)
+            .tint(.purple)
+            
+            Text(textTitle)
+            .padding(20)
         }
-        .padding()
     }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
